7dbf18038355e0eb722378fb30073f7e30eb6a05
Chitadwi <7788chita@gmail.com>
2021-04-27 15:58:59 +0900

    Bottom Menu Navigation Fragment App

---
+++ app/src/main/java/com/example/bottomnavigationmenufragment/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.bottomnavigationmenufragment;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Switch;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
+    private BottomNavigationView.OnNavigationItemSelectedListener navigation = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(MenuItem item) {
+            Fragment f = null;
+            switch (item.getItemId()){
+                case R.id.menu_home:
+                    f = new FragmentHome();
+                    break;
+                case R.id.menu_notifications:
+                    f = new FragmentNotifications();
+                    break;
+                case R.id.menu_cart:
+                    f = new FragmentCart();
+                    break;
+                case R.id.menu_profil:
+                    f = new FragmentProfil();
+                    break;
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.container_fragment,f).commit();
+            return true;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bottomNavigationView = findViewById(R.id.bottom_navigation_menu);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navigation);
+    }
+}
